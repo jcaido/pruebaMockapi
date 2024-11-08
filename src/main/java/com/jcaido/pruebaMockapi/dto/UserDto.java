3442194d03250e212e07cd207ca2b4280c0b40d3
Cristobal Rosa <crisrogar@gmail.com>
2024-11-08 11:18:35 +0100

    create userDto

---
+++ src/main/java/com/jcaido/pruebaMockapi/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.jcaido.pruebaMockapi.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private Integer id;
+    private String name;
+    private String username;
+    private String email;
+}
