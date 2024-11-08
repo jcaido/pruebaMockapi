8f8f5deb37b5d42f960ec3b9ff4d423071eac636
Cristobal Rosa <crisrogar@gmail.com>
2024-11-08 11:08:58 +0100

    first commit

---
+++ src/main/java/com/jcaido/pruebaMockapi/PruebaMockapiApplication.java
@@ -0,0 +1,13 @@
+package com.jcaido.pruebaMockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaMockapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaMockapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcaido/pruebaMockapi/PruebaMockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaido.pruebaMockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaMockapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
