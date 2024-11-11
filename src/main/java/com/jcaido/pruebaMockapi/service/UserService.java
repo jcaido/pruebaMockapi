cfed59d2f8082314a963ef20f0df640d03c508c3
Cristobal Rosa <crisrogar@gmail.com>
2024-11-11 11:26:39 +0100

    create method getAllUsers, userService

---
+++ src/main/java/com/jcaido/pruebaMockapi/service/UserService.java
@@ -0,0 +1,16 @@
+package com.jcaido.pruebaMockapi.service;
+
+import com.jcaido.pruebaMockapi.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public List<UserDto> getAllUsers() {
+        return null;
+    }
+
+
+}
