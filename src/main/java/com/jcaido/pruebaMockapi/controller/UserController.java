aedc2fe730e9e7d92632ad009fc912ecaf6fc3ed
Cristobal Rosa <crisrogar@gmail.com>
2024-11-11 11:13:27 +0100

    create method saveUser, userController

--- src/main/java/com/jcaido/pruebaMockapi/controller/UserController.java
+++ src/main/java/com/jcaido/pruebaMockapi/controller/UserController.java
@@ -3,9 +3,7 @@ package com.jcaido.pruebaMockapi.controller;
 import com.jcaido.pruebaMockapi.dto.UserDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,5 +14,11 @@ public class UserController {
     @GetMapping()
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveUser(@RequestBody UserDto user) {
+
     }
 }
