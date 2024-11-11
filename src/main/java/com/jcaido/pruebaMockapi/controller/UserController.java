4be3480fcfca4b0991e5accfc9dc4e9d6098be49
Cristobal Rosa <crisrogar@gmail.com>
2024-11-11 11:37:07 +0100

    update method getAllUsers, userController

--- src/main/java/com/jcaido/pruebaMockapi/controller/UserController.java
+++ src/main/java/com/jcaido/pruebaMockapi/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.jcaido.pruebaMockapi.controller;
 
 import com.jcaido.pruebaMockapi.dto.UserDto;
+import com.jcaido.pruebaMockapi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +12,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/vi/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping()
     public ResponseEntity<List<UserDto>> getAllUsers() {
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @PostMapping()
