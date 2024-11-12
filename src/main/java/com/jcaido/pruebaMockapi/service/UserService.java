bf8783dc8ce55f9b1b9a3d6fc3e98393193cde56
Cristobal Rosa <crisrogar@gmail.com>
2024-11-12 11:19:19 +0100

    update method getAllUsers, UserService

--- src/main/java/com/jcaido/pruebaMockapi/service/UserService.java
+++ src/main/java/com/jcaido/pruebaMockapi/service/UserService.java
@@ -2,9 +2,11 @@ package com.jcaido.pruebaMockapi.service;
 
 import com.jcaido.pruebaMockapi.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -13,8 +15,14 @@ public class UserService {
 
     private final RestTemplate restTemplate;
 
+    @Value("${spring.external.service.base-url}")
+    private String basePath;
+
     public List<UserDto> getAllUsers() {
-        return null;
+
+        UserDto[] users = restTemplate.getForObject(basePath + "/users", UserDto[].class);
+
+        return Arrays.asList(users);
     }
 
     public void saveUser(UserDto user) {
