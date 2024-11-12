92fb7c3d830739b2e75bfaaf96826dade858166b
Cristobal Rosa <crisrogar@gmail.com>
2024-11-12 10:43:42 +0100

    RestTemplate dependency injection, userService

--- src/main/java/com/jcaido/pruebaMockapi/service/UserService.java
+++ src/main/java/com/jcaido/pruebaMockapi/service/UserService.java
@@ -1,12 +1,17 @@
 package com.jcaido.pruebaMockapi.service;
 
 import com.jcaido.pruebaMockapi.dto.UserDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final RestTemplate restTemplate;
 
     public List<UserDto> getAllUsers() {
         return null;
