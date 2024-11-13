3b07e9dfed71c73bb6ee28505e709dbcc1a7595c
Cristobal Rosa <crisrogar@gmail.com>
2024-11-13 11:45:36 +0100

    update POJO ErrorDetails

--- src/main/java/com/jcaido/pruebaMockapi/exception/ErrorDatails.java
+++ src/main/java/com/jcaido/pruebaMockapi/exception/ErrorDatails.java
@@ -1,8 +1,12 @@
 package com.jcaido.pruebaMockapi.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorDatails {
     private String message;
     private String details;
