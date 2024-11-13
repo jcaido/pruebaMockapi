377d99b6472fb7b7e56b76d0eced4018aa311636
Cristobal Rosa <crisrogar@gmail.com>
2024-11-13 12:58:31 +0100

    update GlobalExceptionHandler

--- src/main/java/com/jcaido/pruebaMockapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jcaido/pruebaMockapi/exception/GlobalExceptionHandler.java
@@ -1,2 +1,25 @@
-package com.jcaido.pruebaMockapi.exception;public class GlobalExceptionHandler {
+package com.jcaido.pruebaMockapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceAlreadyExistException.class)
+    public ResponseEntity<ErrorDatails> handleResourceAlreadyExistException(ResourceAlreadyExistException exception, WebRequest webRequest) {
+        ErrorDatails errorDatails = new ErrorDatails(exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDatails, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDatails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorDatails errorDatails = new ErrorDatails(exception.getMessage(), webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDatails, HttpStatus.NOT_FOUND);
+    }
 }
