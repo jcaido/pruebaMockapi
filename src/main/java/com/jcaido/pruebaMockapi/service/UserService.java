4cd8fcb9c17714c3f046e8b31b571204fb28f286
Cristobal Rosa <crisrogar@gmail.com>
2024-11-14 11:15:05 +0100

    Validations method updateUser, UserService

--- src/main/java/com/jcaido/pruebaMockapi/service/UserService.java
+++ src/main/java/com/jcaido/pruebaMockapi/service/UserService.java
@@ -2,9 +2,12 @@ package com.jcaido.pruebaMockapi.service;
 
 import com.jcaido.pruebaMockapi.dto.UserDto;
 import com.jcaido.pruebaMockapi.exception.ResourceAlreadyExistException;
+import com.jcaido.pruebaMockapi.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -44,6 +47,21 @@ public class UserService {
     }
 
     public void updateUser(Integer id, UserDto user) {
+
+        List<UserDto> users = Arrays.asList(restTemplate.getForObject(basePath + "/users", UserDto[].class));
+
+        boolean userIdNotExist = users.stream().noneMatch(userMatch -> userMatch.getId() == id);
+        if (userIdNotExist)
+            throw new ResourceNotFoundException("user", "id", String.valueOf(id));
+
+        boolean userNameAlreadyExist = users.stream().noneMatch(userMatch -> userMatch.getUsername().equals(user.getUsername()));
+        if (!userNameAlreadyExist)
+            throw new ResourceAlreadyExistException("user", "username", String.valueOf(user.getUsername()));
+
+        boolean emailAlreadyExist = users.stream().noneMatch(userMatch -> userMatch.getEmail().equals(user.getEmail()));
+        if (!emailAlreadyExist)
+            throw new ResourceAlreadyExistException("user", "email", String.valueOf(user.getEmail()));
+
         restTemplate.put(basePath + "/users/" + id, user);
     }
 
